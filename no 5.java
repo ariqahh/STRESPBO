813f610b34beaaccb5d3fe7a685dfccd6b991b67
ANDIALVIANSYAH <147214020+ANDIALVIANSYAH@users.noreply.github.com>
2024-02-29 18:30:53 +0800

    no 5.java

---
+++ no 5.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+public class number5 {
+    public static void main(String[] args) {
+        Vector datavec = new Vector();
+        datavec.add(12);
+        datavec.add(46.4138729);
+        datavec.add("Gajah");
+        datavec.add('j');
+        datavec.add(24);
+        datavec.add('#');
+        datavec.add("jerapah");
+        datavec.add(22.7);
+        System.out.println("DATA VECTOR : "+datavec);
+    }
+}
